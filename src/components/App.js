0977fc086930b650ca39aa7a94b7b834968a4cae
ibsa-ejeta <ibsa.ej@gmail.com>
2021-07-16 10:06:41 -0400

    package.json updated again

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,12 @@
 import React from "react";
 
-import { BrowserRouter, Route, Router, Switch } from "react-router-dom";
+import {
+  BrowserRouter,
+  Route,
+  Router,
+  Switch,
+  HashRouter,
+} from "react-router-dom";
 import Home from "./homeComponents/Home";
 import Tribute from "./Tribute/Tribute";
 import Landing from "./Landing/Landing";
@@ -33,20 +39,18 @@ const theme = createTheme({
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
-      <Router>
-        <Switch>
-          <Route path="/REACT-Profile" exact component={Home} />
-          <Route path="/REACT-Profile/blog" exact component={Blog} />
-          <Route path="/REACT-Profile/Landing_Page" exact component={Landing} />
-          <Route
-            path="/REACT-Profile/Technical_Documentation"
-            exact
-            component={Technical}
-          />
-          <Route path="/REACT-Profile/Survey_Form" exact component={Survey} />
-          <Route path="/REACT-Profile/Tribute_Page" exact component={Tribute} />
-        </Switch>
-      </Router>
+      <HashRouter>
+        <Route path="/REACT-Profile" exact component={Home} />
+        <Route path="/REACT-Profile/blog" exact component={Blog} />
+        <Route path="/REACT-Profile/Landing_Page" exact component={Landing} />
+        <Route
+          path="/REACT-Profile/Technical_Documentation"
+          exact
+          component={Technical}
+        />
+        <Route path="/REACT-Profile/Survey_Form" exact component={Survey} />
+        <Route path="/REACT-Profile/Tribute_Page" exact component={Tribute} />
+      </HashRouter>
     </ThemeProvider>
   );
 };
