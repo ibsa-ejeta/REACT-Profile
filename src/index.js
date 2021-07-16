6463a8af82545834ddbc38b64013bc8dddce76ab
ibsa-ejeta <ibsa.ej@gmail.com>
2021-07-16 09:27:49 -0400

    package.json updated again

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import React from "react";
 import reactDOM from "react-dom";
 import App from "./components/App";
+import { BrowserRouter } from "react-router-dom";
 
-reactDOM.render(<App />, document.getElementById("root"));
+reactDOM.render(
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
+
+
+
+
